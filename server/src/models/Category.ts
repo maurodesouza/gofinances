1b1aacb7bf3da51cb8a2bb2508b32b7f8c1ffd7c
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-02 01:04:45 -0300

    ⚡  chore(server): add categories migration

--- server/src/models/Category.ts
+++ server/src/models/Category.ts
@@ -1,10 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import Transaction from './Transaction';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @OneToMany(() => Transaction, trasaction => trasaction.category)
+  transactions: Transaction[];
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
