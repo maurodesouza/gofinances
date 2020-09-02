4793fe53295a973e833e0cbc05dae1f960a3392b
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-02 01:09:45 -0300

    ✨  feat(server): add route to create transaction

--- server/src/routes/transactions.routes.ts
+++ server/src/routes/transactions.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -12,7 +12,18 @@ transactionsRouter.get('/', async (request, response) => {
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, type, category, value } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const newTransaction = await createTransaction.execute({
+    title,
+    type,
+    value,
+    category,
+  });
+
+  return response.status(201).json(newTransaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
