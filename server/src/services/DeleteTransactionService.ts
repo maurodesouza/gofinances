683d3bb36f8ce493d997712c124924c5a86f3c81
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-02 01:23:36 -0300

    ⚡  chore(server): add delete transaction service

--- server/src/services/DeleteTransactionService.ts
+++ server/src/services/DeleteTransactionService.ts
@@ -1,8 +1,20 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface RequestDTO {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: RequestDTO): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) throw new AppError('Transação não encontrada.');
+
+    await transactionRepository.delete(id);
   }
 }
 
