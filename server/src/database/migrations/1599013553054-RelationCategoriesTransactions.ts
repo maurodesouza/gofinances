fb009dc92f0f1c16543c434992acda916a4b26ab
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-02 01:07:27 -0300

    ⚡  chore(server): add relation categories and transactions

---
+++ server/src/database/migrations/1599013553054-RelationCategoriesTransactions.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class RelationCategoriesTransactions1599013553054
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'CategoriesTransactions',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'CategoriesTransactions');
+  }
+}
